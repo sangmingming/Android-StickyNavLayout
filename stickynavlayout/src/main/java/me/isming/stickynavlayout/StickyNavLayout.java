2dd28063c7dc5655f4fb5a546dcae0eace09c19b
Sang Mingming <msang@xingin.com>
2015-11-19 10:00:35 +0800

    first commit

---
+++ stickynavlayout/src/main/java/me/isming/stickynavlayout/StickyNavLayout.java
@@ -0,0 +1,344 @@
+package me.isming.stickynavlayout;
+
+/**
+ * Created by sam on 15/11/19.
+ */
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.OverScroller;
+import android.widget.ScrollView;
+
+public class StickyNavLayout extends LinearLayout {
+
+    private View mTop;
+    private View mNav;
+    //private ViewPager mViewPager;
+    //private View mNotViewPager;
+    private int mTopViewHeight;
+    private ViewGroup mInnerScrollView;
+    private boolean isTopHidden = false;
+    private View mBottomView;
+
+    private OverScroller mScroller;
+    private VelocityTracker mVelocityTracker;
+    private int mTouchSlop;
+    private int mMaximumVelocity, mMinimumVelocity;
+
+    private float mLastY;
+    private boolean mDragging;
+    private int mLastScrollY;
+
+    private boolean isInControl = false;
+    private boolean isSticky;  //mNav-view 是否悬停的标志
+
+    private OnHeaderStatusChange mListener;
+
+    public StickyNavLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(LinearLayout.VERTICAL);
+
+        mScroller = new OverScroller(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mMaximumVelocity = ViewConfiguration.get(context)
+                .getScaledMaximumFlingVelocity();
+        mMinimumVelocity = ViewConfiguration.get(context)
+                .getScaledMinimumFlingVelocity();
+
+    }
+
+    public void setOnHeadStatusChangeListener(OnHeaderStatusChange listener) {
+        mListener = listener;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        if (getChildCount() < 3) {
+            throw new RuntimeException("StickyNavLayout must three child view");
+        }
+        mTop = getChildAt(0);
+        mNav = getChildAt(1);
+        mBottomView = getChildAt(2);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        ViewGroup.LayoutParams params = mBottomView.getLayoutParams();
+        mTopViewHeight = mTop.getMeasuredHeight();
+        params.height = getMeasuredHeight() - mNav.getMeasuredHeight();
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mTopViewHeight = mTop.getMeasuredHeight();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        float y = ev.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mLastY;
+                getCurrentScrollView();
+
+                if (mInnerScrollView instanceof ScrollView) {
+                    if (mInnerScrollView.getScrollY() == 0 && isTopHidden && dy > 0
+                            && !isInControl) {
+                        isInControl = true;
+                        isSticky = true;
+                        ev.setAction(MotionEvent.ACTION_CANCEL);
+                        MotionEvent ev2 = MotionEvent.obtain(ev);
+                        dispatchTouchEvent(ev);
+                        ev2.setAction(MotionEvent.ACTION_DOWN);
+                        return dispatchTouchEvent(ev2);
+                    }
+                } else if (mInnerScrollView instanceof ListView) {
+
+                    ListView lv = (ListView) mInnerScrollView;
+                    View c = lv.getChildAt(lv.getFirstVisiblePosition());
+
+                    if (!isInControl && c != null && c.getTop() == 0 && isTopHidden
+                            && dy > 0) {
+                        isInControl = true;
+                        isSticky = true;
+                        ev.setAction(MotionEvent.ACTION_CANCEL);
+                        MotionEvent ev2 = MotionEvent.obtain(ev);
+                        dispatchTouchEvent(ev);
+                        ev2.setAction(MotionEvent.ACTION_DOWN);
+                        return dispatchTouchEvent(ev2);
+                    }
+                } else if (mInnerScrollView instanceof RecyclerView) {
+                    RecyclerView rv = (RecyclerView) mInnerScrollView;
+
+                    if (!isInControl && android.support.v4.view.ViewCompat.canScrollVertically(rv, -1) && isTopHidden
+                            && dy > 0) {
+                        isInControl = true;
+                        isSticky = true;
+                        ev.setAction(MotionEvent.ACTION_CANCEL);
+                        MotionEvent ev2 = MotionEvent.obtain(ev);
+                        dispatchTouchEvent(ev);
+                        ev2.setAction(MotionEvent.ACTION_DOWN);
+                        return dispatchTouchEvent(ev2);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                float distance = y - mLastY;
+                if (isSticky && Math.abs(distance) <= mTouchSlop) {
+                    isSticky = false;
+                    return true;
+                } else {
+                    isSticky = false;
+                }
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = ev.getAction();
+        float y = ev.getY();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mLastY;
+                getCurrentScrollView();
+                if (Math.abs(dy) > mTouchSlop) {
+                    mDragging = true;
+                    if (mInnerScrollView instanceof ScrollView) {
+                        // 如果topView没有隐藏
+                        // 或sc的scrollY = 0 && topView隐藏 && 下拉，则拦截
+                        if ((!isTopHidden && (dy < 0 || mInnerScrollView.getScrollY() == 0))
+                                || (mInnerScrollView.getScrollY() == 0
+                                && isTopHidden && dy > 0)) {
+
+                            initVelocityTrackerIfNotExists();
+                            mVelocityTracker.addMovement(ev);
+                            mLastY = y;
+                            return true;
+                        }
+                    } else if (mInnerScrollView instanceof ListView) {
+
+                        ListView lv = (ListView) mInnerScrollView;
+                        View c = lv.getChildAt(lv.getFirstVisiblePosition());
+                        // 如果topView没有隐藏
+                        // 或sc的listView在顶部 && topView隐藏 && 下拉，则拦截
+
+                        if ((!isTopHidden && (dy < 0 || (c != null && c.getTop() ==0))) || //
+                                (c != null //
+                                        && c.getTop() == 0//
+                                        && isTopHidden && dy > 0)) {
+
+                            initVelocityTrackerIfNotExists();
+                            mVelocityTracker.addMovement(ev);
+                            mLastY = y;
+                            return true;
+                        }
+                    } else if (mInnerScrollView instanceof RecyclerView) {
+                        RecyclerView rv = (RecyclerView) mInnerScrollView;
+                        if ((!isTopHidden && (dy < 0 || !android.support.v4.view.ViewCompat.canScrollVertically(rv, -1))) || (!android.support.v4.view.ViewCompat.canScrollVertically(rv, -1) && isTopHidden && dy > 0)) {
+                            initVelocityTrackerIfNotExists();
+                            mVelocityTracker.addMovement(ev);
+                            mLastY = y;
+                            return true;
+                        }
+                    }
+
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                mDragging = false;
+                recycleVelocityTracker();
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    private void getCurrentScrollView() {
+        if (mBottomView instanceof  ViewPager) {
+            ViewPager pager = (ViewPager) mBottomView;
+            int currentItem = pager.getCurrentItem();
+            mInnerScrollView = (ViewGroup) pager.getChildAt(currentItem);
+        } else {
+            if (mInnerScrollView == null) {
+                mInnerScrollView = (ViewGroup) mBottomView;
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        initVelocityTrackerIfNotExists();
+        mVelocityTracker.addMovement(event);
+        int action = event.getAction();
+        float y = event.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (!mScroller.isFinished())
+                    mScroller.abortAnimation();
+                mLastY = y;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float dy = y - mLastY;
+
+                if (!mDragging && Math.abs(dy) > mTouchSlop) {
+                    mDragging = true;
+                }
+                if (mDragging) {
+                    scrollBy(0, (int) -dy);
+
+                    // 如果topView隐藏，且上滑动时，则改变当前事件为ACTION_DOWN
+                    if (getScrollY() == mTopViewHeight && dy < 0) {
+                        event.setAction(MotionEvent.ACTION_DOWN);
+                        dispatchTouchEvent(event);
+                        isInControl = false;
+                        isSticky = true;
+                    } else {
+                        isSticky = false;
+                    }
+                }
+
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                mDragging = false;
+                recycleVelocityTracker();
+                if (!mScroller.isFinished()) {
+                    mScroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mDragging = false;
+                mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
+                int velocityY = (int) mVelocityTracker.getYVelocity();
+                if (Math.abs(velocityY) > mMinimumVelocity) {
+                    fling(-velocityY);
+                }
+                recycleVelocityTracker();
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    public void fling(int velocityY) {
+        mScroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, mTopViewHeight);
+        invalidate();
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        if (y < 0) {
+            y = 0;
+        }
+        if (y > mTopViewHeight) {
+            y = mTopViewHeight;
+        }
+        if (y != getScrollY()) {
+            super.scrollTo(x, y);
+        }
+
+        boolean oldStatus = isTopHidden;
+        isTopHidden = getScrollY() == mTopViewHeight;
+
+        if (oldStatus != isTopHidden && mListener != null) {
+            mListener.onStatusChange(!isTopHidden);
+        }
+
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(0, mScroller.getCurrY());
+            invalidate();
+        }
+    }
+
+    private void initVelocityTrackerIfNotExists() {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    private void recycleVelocityTracker() {
+        if (mVelocityTracker != null) {
+            mVelocityTracker.recycle();
+            mVelocityTracker = null;
+        }
+    }
+
+    public interface OnHeaderStatusChange {
+        void onStatusChange(boolean isShow);
+    }
+
+
+}
